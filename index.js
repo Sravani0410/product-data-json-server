87603db1057a03261ea188a0eb9bc318075f8ab7
sravani <sandhyapyla.8@gmail.com>
2024-07-05 07:47:23 +0530

    add cors and test

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 8080; 
+const cors = require('cors');
+server.use(cors());
 
 server.use(middlewares);
 server.use(router);
 
-server.listen(port);
+server.listen(port, () => {
+  console.log(`JSON Server is running on http://localhost:${port}`);
+});
